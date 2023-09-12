8c9e2bbdad3c4c96d9b686143310721fa8e26469
yeomheesu1 <yeomheesu@chunjae.co.kr>
2023-09-12 09:51:06 +0900

    admin template 적용

---
+++ src/menuAside.js
@@ -0,0 +1,111 @@
+import {
+  mdiAccountCircle,
+  mdiMonitor,
+  mdiGithub,
+  mdiLock,
+  mdiAlertCircle,
+  mdiSquareEditOutline,
+  mdiTable,
+  mdiViewList,
+  mdiTelevisionGuide,
+  mdiResponsive,
+  mdiPalette,
+  mdiReact,
+} from "@mdi/js";
+
+export default [
+  {
+    to: "/dashboard",
+    icon: mdiMonitor,
+    label: "Dashboard",
+  },
+  {
+    label: "콘텐츠 관리A",
+    icon: mdiViewList,
+    menu: [
+      {
+        label: "책묶음 관리",
+      },
+      {
+        label: "책관리",
+      },
+      {
+        label: "문항 관리",
+      },
+      {
+        label: "개념 관리",
+      },
+    ],
+  },
+  {
+    label: "콘텐츠 관리B",
+    icon: mdiViewList,
+    menu: [
+      {
+        label: "책묶음 관리",
+      },
+      {
+        label: "책관리",
+      },
+    ],
+  },
+  // {
+  //   to: "/forms",
+  //   label: "Forms",
+  //   icon: mdiSquareEditOutline,
+  // },
+  // {
+  //   to: "/ui",
+  //   label: "UI",
+  //   icon: mdiTelevisionGuide,
+  // },
+  // {
+  //   to: "/responsive",
+  //   label: "Responsive",
+  //   icon: mdiResponsive,
+  // },
+  // {
+  //   to: "/",
+  //   label: "Styles",
+  //   icon: mdiPalette,
+  // },
+  // {
+  //   to: "/profile",
+  //   label: "Profile",
+  //   icon: mdiAccountCircle,
+  // },
+  // {
+  //   to: "/login",
+  //   label: "Login",
+  //   icon: mdiLock,
+  // },
+  // {
+  //   to: "/error",
+  //   label: "Error",
+  //   icon: mdiAlertCircle,
+  // },
+  // {
+  //   label: "Dropdown",
+  //   icon: mdiViewList,
+  //   menu: [
+  //     {
+  //       label: "Item One",
+  //     },
+  //     {
+  //       label: "Item Two",
+  //     },
+  //   ],
+  // },
+  // {
+  //   href: "https://github.com/justboil/admin-one-vue-tailwind",
+  //   label: "GitHub",
+  //   icon: mdiGithub,
+  //   target: "_blank",
+  // },
+  // {
+  //   href: "https://github.com/justboil/admin-one-react-tailwind",
+  //   label: "React version",
+  //   icon: mdiReact,
+  //   target: "_blank",
+  // },
+];
---
+++ src/menuNavBar.js
@@ -0,0 +1,56 @@
+import {
+  mdiMenu,
+  mdiClockOutline,
+  mdiCloud,
+  mdiCrop,
+  mdiAccount,
+  mdiCogOutline,
+  mdiEmail,
+  mdiLogout,
+  mdiThemeLightDark,
+  mdiGithub,
+  mdiReact,
+} from "@mdi/js";
+
+export default [
+  {
+    icon: mdiMenu,
+    label: "Sample menu",
+    menu: [
+      {
+        icon: mdiClockOutline,
+        label: "Item One",
+      },
+      {
+        icon: mdiCloud,
+        label: "Item Two",
+      },
+      {
+        isDivider: true,
+      },
+      {
+        icon: mdiCrop,
+        label: "Item Last",
+      },
+    ],
+  },
+  {
+    isCurrentUser: true,
+    menu: [
+
+      {
+        icon: mdiCogOutline,
+        label: "Settings",
+      },
+
+      {
+        isDivider: true,
+      },
+      {
+        icon: mdiLogout,
+        label: "Log Out",
+        isLogout: true,
+      },
+    ],
+  },
+];
